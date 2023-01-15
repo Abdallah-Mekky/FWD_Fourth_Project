d30a3d7445706dc020ece86c91d0e73500928362
Abdallah-Mekky <abdallahmekky117@gmail.com>
2023-01-15 16:47:15 +0200

    Add Select Reminder Location Feature

---
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationConstants.kt
@@ -0,0 +1,8 @@
+package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
+
+object SelectLocationConstants {
+
+     const val REQUEST_CODE_BACKGROUND = 20
+     const val REQUEST_TURN_DEVICE_LOCATION_ON = 8
+     const val REQUEST_LOCATION_PERMISSION = 2001
+}
