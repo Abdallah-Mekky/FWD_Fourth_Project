1b7fb9cae9b9c86635b0850d0e0355327c752b63
Abdallah-Mekky <abdallahmekky117@gmail.com>
2023-01-15 03:19:33 +0200

    Add Authentication Feature

---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,91 @@
+package com.udacity.project4.authentication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
+import kotlinx.android.synthetic.main.activity_authentication.*
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var signInLauncher: ActivityResultLauncher<Intent>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+        login_And_Register.setOnClickListener {
+
+            signIn()
+        }
+        signInLauncher = registerForActivityResult(
+            FirebaseAuthUIActivityResultContract()
+        ) { result ->
+            onSignInResult(result)
+
+        }
+
+        startSignIn()
+
+
+    }
+
+    private fun goToRemindersActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+
+            Toast.makeText(this, "Successfully Signed In", Toast.LENGTH_LONG).show()
+
+        } else {
+            Toast.makeText(this, "something wrong \n please try again", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun startSignIn() {
+        loginViewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    goToRemindersActivity()
+                }
+                LoginViewModel.AuthenticationState.UNAUTHENTICATED -> {
+                    login_And_Register.setOnClickListener {
+                        signIn()
+                    }
+                }
+                else -> {}
+            }
+        })
+
+
+    }
+
+    private fun signIn() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.AppTheme)
+            .build()
+
+        signInLauncher.launch(signInIntent)
+    }
+}
